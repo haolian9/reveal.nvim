93a31e077b812e20ff27d97b74b27d5fec017b61
haoliang <haoliang0.1.2@gmail.com>
2022-10-23 20:49:06 +0800

    cleanup unused functions

--- vifm/init.lua
+++ vifm/init.lua
@@ -14,22 +14,5 @@ vifm.addhandler({
   handler = openers.open,
 })
 
-if false then
-  local viewers = require("viewers")(shared)
-  local vicmd = require("vicmd")(shared)
-  vifm.addhandler({
-    name = "view",
-    handler = viewers.show,
-  })
-
-  vifm.addhandler({
-    name = "vicmd",
-    handler = function(info)
-      vifm.sb.err(string.format("vicmd.action: %s", info.action))
-      return vicmd(info)
-    end,
-  })
-end
-
 -- vifm always expects a table returned from a plugin/init.lua
 return {}
